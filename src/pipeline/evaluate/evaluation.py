7c8c05dd75e18ed4c41ca592c23f021fb1cb1b1e
Gaétan Ramet <gr@visium.ch>
2024-04-23 18:57:11 +0200

    first iteration of the pipeline

--- src/pipeline/evaluate/evaluation.py
+++ src/pipeline/evaluate/evaluation.py
@@ -1,12 +1,36 @@
 """Module for model evaluation."""
 
+import pickle
 from pathlib import Path
+from statistics import mean
 
+import pandas as pd
 import typer
+from sklearn.metrics import mean_absolute_error
 
 
 def main(preprocess_folder: Path = typer.Option(...), train_folder: Path = typer.Option(...)) -> None:
     """Main function."""
+    df_train = pd.read_parquet(preprocess_folder / "df_train.parquet")
+    df_test = pd.read_parquet(preprocess_folder / "df_test.parquet")
+
+    with open(train_folder / "model.pkl", "rb") as f:
+        model = pickle.load(f)
+    target_column = "average_grade"
+    X_train = df_train.drop(columns=[target_column])
+    y_train = df_train[target_column]
+    model.fit(X_train, y_train)
+
+    X_test = df_test.drop(columns=[target_column])
+    y_test = df_test[target_column]
+    predictions = model.predict(X_test)
+
+    baseline_pred = y_train.mean()
+
+    mae = mean_absolute_error(y_test, predictions)
+    mae_baseline = mean_absolute_error(y_test, [baseline_pred] * len(y_test))
+
+    print(f"{mae=}, {mae_baseline=}")
 
 
 if __name__ == "__main__":
--- src/pipeline/preprocess/preprocessing.py
+++ src/pipeline/preprocess/preprocessing.py
@@ -2,12 +2,30 @@
 
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
 import typer
+from sklearn.model_selection import train_test_split
 
 
 def main(input_folder: Path = typer.Option(...), output_folder: Path = typer.Option(...)) -> None:
     """Main function."""
     output_folder.mkdir(exist_ok=True, parents=True)
+
+    df = pd.read_csv(input_folder / "student-por.csv")
+
+    categorical_variables = [col for col, dtype in df.items() if not np.issubdtype(dtype, np.number)]
+    df = pd.get_dummies(df, columns=categorical_variables, drop_first=False, dtype=float)
+
+    df["average_grade"] = (df["G1"] + df["G2"] + df["G3"]) / 3.0
+    df = df.drop(["G1", "G2", "G3"], axis="columns")
+
+    df_train, df_test = train_test_split(df, test_size=0.2)
+    df_train, df_val = train_test_split(df_train, test_size=0.2)
+
+    df_train.to_parquet(output_folder / "df_train.parquet")
+    df_val.to_parquet(output_folder / "df_val.parquet")
+    df_test.to_parquet(output_folder / "df_test.parquet")
 
 
 if __name__ == "__main__":
