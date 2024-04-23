85cc83a930010d4bb31f85ced9bd3ab5737905cf
Gaétan Ramet <gr@visium.ch>
2024-04-23 14:18:17 +0200

    added output folder creation

--- src/pipeline/train/training.py
+++ src/pipeline/train/training.py
@@ -7,6 +7,7 @@ import typer
 
 def main(preprocess_folder: Path = typer.Option(...), output_folder: Path = typer.Option(...)) -> None:
     """Main function."""
+    output_folder.mkdir(exist_ok=True, parents=True)
 
 
 if __name__ == "__main__":
